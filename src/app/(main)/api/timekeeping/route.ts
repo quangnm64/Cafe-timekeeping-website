c3e7eeb5f4c5708689baff3c09c1c96c504035a0
quangnm64 <quang.nm.64cntt@ntu.edu.vn>
2025-12-19 11:20:13 +0700

    chỉnh sửa form chấm công

--- src/app/(main)/api/timekeeping/route.ts
+++ src/app/(main)/api/timekeeping/route.ts
@@ -3,8 +3,7 @@ import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import prisma from '../../../../../prisma/prismaClient';
 
-export async function POST(req: Request) {
-  const time = req.json();
+async function CheckIn() {
   try {
     const token = (await cookies()).get('access_token')?.value;
 
@@ -26,6 +25,7 @@ export async function POST(req: Request) {
     if (result) {
       return NextResponse.json({
         status: true,
+        resut: result,
       });
     }
     return NextResponse.json({
@@ -35,4 +35,124 @@ export async function POST(req: Request) {
     console.error('Create attendance error:', error);
     throw error;
   }
+}
+async function CheckOut() {
+  try {
+    const token = (await cookies()).get('access_token')?.value;
+
+    if (!token) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+
+    const JWT_SECRET = process.env.JWT_TOKEN_SECRET!;
+    const user = jwt.verify(token, JWT_SECRET) as jwt.JwtPayload;
+    const today = new Date();
+    today.setUTCHours(0, 0, 0, 0);
+    const att = await prisma.attendance.findFirst({
+      where: {
+        employee_id: BigInt(user.employee_id),
+        work_date: {
+          gte: today,
+        },
+      },
+    });
+    if (att?.attendance_id) {
+      const result = await prisma.attendance.update({
+        where: {
+          attendance_id: BigInt(att?.attendance_id),
+          employee_id: BigInt(user.employee_id),
+          work_date: {
+            gte: today,
+          },
+        },
+        data: {
+          check_out: new Date(),
+        },
+      });
+      if (result) {
+        return NextResponse.json({
+          status: true,
+          resut: {
+            ...result,
+            attendance_id: result.attendance_id.toString(),
+            employee_id: result.employee_id.toString(),
+          },
+        });
+      }
+    }
+    return NextResponse.json({
+      status: false,
+    });
+  } catch (error) {
+    console.error('Create attendance error:', error);
+    throw error;
+  }
+}
+export async function GET() {
+  try {
+    const token = (await cookies()).get('access_token')?.value;
+
+    if (!token) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+
+    const JWT_SECRET = process.env.JWT_TOKEN_SECRET!;
+    const user = jwt.verify(token, JWT_SECRET) as jwt.JwtPayload;
+    const today = new Date('2025-12-19T00:00:00.993Z');
+    const result = await prisma.attendance.findFirst({
+      where: {
+        employee_id: BigInt(user.employee_id),
+        work_date: {
+          gte: today,
+        },
+      },
+    });
+    if (result) {
+      if (result.check_in) {
+        if (result.check_out) {
+          return NextResponse.json({
+            checkIn: true,
+            checkOut: true,
+            status: true,
+            result: {
+              ...result,
+              attendance_id: result.attendance_id.toString(),
+              employee_id: result.employee_id.toString(),
+            },
+          });
+        }
+        return NextResponse.json({
+          checkIn: true,
+          checkOut: false,
+          status: true,
+          result: {
+            ...result,
+            attendance_id: result.attendance_id.toString(),
+            employee_id: result.employee_id.toString(),
+          },
+        });
+      }
+    }
+
+    return NextResponse.json({
+      checkIn: true,
+      checkOut: false,
+      status: false,
+    });
+  } catch (error) {
+    console.error('Create attendance error:', error);
+    throw error;
+  }
+}
+export async function POST(req: Request) {
+  const task = await req.json();
+  if (task.value === 'checkin') {
+    return CheckIn();
+  }
+  if (task.value === 'checkout') {
+    return CheckOut();
+  }
+  return NextResponse.json({
+    message: 'Error when call',
+  });
 }
--- src/modules/timekeeping/components/form-timekeeping.tsx
+++ src/modules/timekeeping/components/form-timekeeping.tsx
@@ -7,14 +7,22 @@ import { Button } from '@/react-web-ui-shadcn/src/components/ui/button';
 import axios from 'axios';
 
 export function TimekeepingPage() {
-  const [currentTime, setCurrentTime] = useState(new Date());
   const [checkInTime, setCheckInTime] = useState<Date | null>(null);
   const [checkOutTime, setCheckOutTime] = useState<Date | null>(null);
   const [address, setAddress] = useState('');
+  const [lat, setLat] = useState(0);
+  const [long, setLong] = useState(0);
+
+  const formatter = new Intl.DateTimeFormat('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((pos) => {
       const { latitude, longitude } = pos.coords;
-
+      setLat(latitude);
+      setLong(longitude);
       const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
 
       fetch(url)
@@ -25,55 +33,36 @@ export function TimekeepingPage() {
     });
   }, []);
   useEffect(() => {
-    const timer = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000);
-
-    return () => clearInterval(timer);
+    async function fetchStatus() {
+      await axios.get('/api/timekeeping').then((res) => {
+        if (res.data.checkIn)
+          setCheckInTime(new Date(res.data.result.check_in));
+        if (res.data.checkOut)
+          setCheckOutTime(new Date(res.data.result.check_out));
+      });
+    }
+    fetchStatus();
   }, []);
-
   const handleCheckIn = async () => {
-    setCheckInTime(new Date());
-    const now = new Date();
-    await axios.post('/api/timekeeping', now);
-    setCheckInTime(now);
+    await axios.post('/api/timekeeping', { value: 'checkin' });
   };
 
-  const handleCheckOut = () => {
-    setCheckOutTime(new Date());
-  };
-
-  const formatTime = (date: Date) => {
-    return date.toLocaleTimeString('vi-VN', {
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-    });
-  };
-
-  const formatDate = (date: Date) => {
-    return date.toLocaleDateString('vi-VN', {
-      weekday: 'long',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    });
+  const handleCheckOut = async () => {
+    await axios.post('/api/timekeeping', { value: 'checkout' });
   };
 
   return (
     <div className="max-w-4xl mx-auto space-y-6">
-      <Card className="p-8 text-center bg-gradient-to-br from-[#658C58] to-[#31694E] text-white">
+      <Card className="p-8 text-center bg-linear-to-br from-[#658C58] to-[#31694E] text-white">
         <div className="flex items-center justify-center mb-4">
           <Clock className="w-12 h-12 mr-3" />
-          <h2 className="text-2xl font-semibold">
-            {/* {address === 'Lê Quý Đôn Phường Nha Trang Khánh Hòa'
-              ? 'Đã đúng vị trí'
-              : 'Chưa đúng vị trí'} */}
-            {address}
-          </h2>
+          <h2 className="text-2xl font-semibold"></h2>
         </div>
-        <div className="text-6xl font-bold mb-2">{formatTime(currentTime)}</div>
-        <div className="text-xl opacity-90">{formatDate(currentTime)}</div>
+        <div className="text-xl opacity-90">
+          {Math.abs(lat - 12) > 1 ? 'Bạn đã ' : 'đúng vị trí' + ' '}
+          <br></br>
+          {address + ' ' + lat + ' ' + long}
+        </div>
       </Card>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -83,21 +72,9 @@ export function TimekeepingPage() {
               <CheckCircle className="w-8 h-8 text-white" />
             </div>
             <h3 className="text-xl font-semibold mb-2">Chấm Công Vào</h3>
-            {checkInTime ? (
-              <div className="mb-4">
-                <p className="text-sm text-muted-foreground mb-1">
-                  Đã chấm công lúc:
-                </p>
-                <p className="text-2xl font-bold text-[#658C58]">
-                  {formatTime(checkInTime)}
-                </p>
-              </div>
-            ) : (
-              <p className="text-muted-foreground mb-4">Chưa chấm công</p>
-            )}
             <Button
               onClick={handleCheckIn}
-              disabled={!!checkInTime}
+              disabled={checkInTime === null ? false : true}
               className="w-full bg-[#658C58] hover:bg-[#31694E] text-white"
               size="lg"
             >
@@ -112,21 +89,10 @@ export function TimekeepingPage() {
               <CheckCircle className="w-8 h-8 text-[#31694E]" />
             </div>
             <h3 className="text-xl font-semibold mb-2">Chấm Công Ra</h3>
-            {checkOutTime ? (
-              <div className="mb-4">
-                <p className="text-sm text-muted-foreground mb-1">
-                  Đã chấm công lúc:
-                </p>
-                <p className="text-2xl font-bold text-[#658C58]">
-                  {formatTime(checkOutTime)}
-                </p>
-              </div>
-            ) : (
-              <p className="text-muted-foreground mb-4">Chưa chấm công</p>
-            )}
+
             <Button
               onClick={handleCheckOut}
-              disabled={!checkInTime || !!checkOutTime}
+              disabled={checkOutTime === null ? false : true}
               className="w-full bg-[#BBC863] hover:bg-[#658C58] text-white"
               size="lg"
             >
@@ -145,18 +111,18 @@ export function TimekeepingPage() {
           <div className="p-4 rounded-lg bg-muted">
             <p className="text-sm text-muted-foreground mb-1">Giờ Vào</p>
             <p className="text-xl font-bold text-[#658C58]">
-              {checkInTime ? formatTime(checkInTime) : '--:--:--'}
+              {checkInTime ? formatter.format(checkInTime) : ''}
             </p>
           </div>
           <div className="p-4 rounded-lg bg-muted">
             <p className="text-sm text-muted-foreground mb-1">Giờ Ra</p>
             <p className="text-xl font-bold text-[#658C58]">
-              {checkOutTime ? formatTime(checkOutTime) : '--:--:--'}
+              {checkOutTime ? formatter.format(checkOutTime) : ''}
             </p>
           </div>
           <div className="p-4 rounded-lg bg-muted">
             <p className="text-sm text-muted-foreground mb-1">Tổng Giờ Làm</p>
-            <p className="text-xl font-bold text-[#658C58]">
+            {/* <p className="text-xl font-bold text-[#658C58]">
               {checkInTime && checkOutTime
                 ? `${Math.floor(
                     (checkOutTime.getTime() - checkInTime.getTime()) / 3600000
@@ -166,7 +132,7 @@ export function TimekeepingPage() {
                       60000
                   )}m`
                 : '--h --m'}
-            </p>
+            </p> */}
           </div>
         </div>
       </Card>
